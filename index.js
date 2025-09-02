f262df2c859ebb42781e5ed39dc461c89d6d595f
TR4N5P4R3NT <159647064+darlingtonofori@users.noreply.github.com>
2025-09-02 19:51:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -523,7 +523,7 @@ process.on('unhandledRejection', (err) => {
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
     fs.unwatchFile(file)
-    console.log(chalk.redBright(`Update ${__filename`))
+    console.log(chalk.redBright(`Update ${__filename}`))
     delete require.cache[file]
     require(file)
 })
